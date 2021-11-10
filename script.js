d929700b864cf63b46bc77eca8b9762d95492faa
Ankita <ankita.gandhim09@gmail.com>
2021-11-10 12:32:55 +0000

    added plusminus, percentages, clear functions

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const screenDisplay = document.querySelector ("#div_screen")
 let numbers= document.querySelectorAll (".number")
 const operators= document.querySelectorAll (".operator")
 const equals= document.querySelector (".equals")
+const clearcommand= document.querySelector (".clear")
+const plusminuscommand = document.querySelector ("#plusminus")
+const percentagecommand = document.querySelector ("#percentage")
 let firstNum;
 let secondNum;
 let operation; 
+let screenDigits;
 
 //display numbers on screen 
 numbers.forEach((numb)=> {
@@ -14,8 +18,9 @@ numbers.forEach((numb)=> {
     if (operation) {
     screenDisplay.innerHTML= "";
   }
+  
   let digit= event.target.value;
-  screenDisplay.innerHTML+= digit;
+  screenDisplay.innerHTML += digit; 
   
 })} )
 
@@ -41,13 +46,54 @@ if ( operation == "+") {
   screenDisplay.innerHTML = parseFloat(firstNum) - parseFloat(secondNum)
   
   } else if (operation == "÷") {
-    screenDisplay,innerHTML = parseFloat (firstNum) / parseFloat (secondNum)
+    screenDisplay.innerHTML = parseFloat (firstNum) / parseFloat (secondNum)
   }
   else if (operation == "x") {
     screenDisplay.innerHTML = parseFloat(firstNum) * parseFloat (secondNum)
   }
 })  
 
+//clear button
+
+clearcommand.addEventListener ("click", () =>
+{
+  screenDisplay.innerHTML= "";
+  firstNum="";
+  secondNum=""; 
+}) 
+
+
+// plusminus
+plusminuscommand.addEventListener ("click", () => {
+ screenDigits = screenDisplay.innerHTML;
+
+   if (screenDigits > 0) {
+   screenDisplay.innerHTML= screenDigits * (-1)
+   } else if (screenDigits < 0) {
+     screenDisplay.innerHTML = screenDigits * (1)
+   }
+ }) 
+
+// percentages /100
+percentagecommand.addEventListener ("click", () => {
+  screenDigits = screenDisplay.innerHTML;
+  screenDisplay.innerHTML= screenDigits / (100)}
+
+)
+
+
+// {
+//   if (firstNum > 0) 
+//   screenDisplay.innerHTML= parseFloat (firstNum) * (-1) 
+//   else if ( firstNum < 0)
+//   screenDisplay.innerHTML= parseFloat (firstNum) * (1)  
+// }) 
+
+
+
+// //dot command
+// dotcommand.addEventListener ("click", () => {
+//   screenDisplay.innerHTML= firstNum || secondNum +  "."
 
 //   `// display jumber
 // // loop thro numbers
