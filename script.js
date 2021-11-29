700a468de23d31a96bb5c59623e6d131131fb1ac
Ankita Manmoandas <ankitamanmoandas@Ankitas-MacBook-Pro.local>
2021-11-29 19:47:40 +0000

    limited the input numbers to be less than 10 characets, and refactored operator and plus minus functions

--- script.js
+++ script.js
@@ -18,12 +18,13 @@ numbers.forEach((numb)=> {
   numb.addEventListener ("click", (event) => {
     if (screenDisplay.innerHTML==operation) {
     screenDisplay.innerHTML= "";
-  }
-  
-  let digits= event.target.value;
-  screenDisplay.innerHTML += digits; 
-  
-})} )
+    }
+  // limit screen display to 10 digits for first and second numbers. 
+  screenDisplay.innerHTML += event.target.value
+  if (screenDisplay.innerHTML.length >= 9) {
+    event.target.value= "" }
+
+})})
 
 
 // display operators + firstnumber 
@@ -38,26 +39,34 @@ screenDisplay.innerHTML=operation;
 
   // now we need to add second number
   
-equals.addEventListener ("click", () =>
- {secondNum= screenDisplay.innerHTML;
-  if (screenDisplay.innerHTML == "") {
-    secondDisplay.innerHTML == "";
-  } else {secondDisplay.innerHTML= (firstNum) + " " + (operation) +  " " + (secondNum);
-  }
-   
+const calculateResult= (operation) => {
+
 if ( operation == "+") {
-  screenDisplay.innerHTML = parseFloat(firstNum) + parseFloat (secondNum) 
-} else if ( operation == "-") {
-  screenDisplay.innerHTML = parseFloat(firstNum) - parseFloat(secondNum)
-  
+  return parseFloat(firstNum) + parseFloat(secondNum)
+  } else if ( operation == "-") {
+  return parseFloat(firstNum) - parseFloat(secondNum)
   } else if (operation == "÷") {
-    screenDisplay.innerHTML = parseFloat (firstNum) / parseFloat (secondNum)
+   return parseFloat (firstNum) / parseFloat (secondNum)
   }
   else if (operation == "x") {
-    screenDisplay.innerHTML = parseFloat(firstNum) * parseFloat (secondNum)
+    return parseFloat(firstNum) * parseFloat (secondNum)
   }
+}
 
-})  
+equals.addEventListener("click", ()  => {
+  secondNum= screenDisplay.innerHTML;
+{ 
+  if 
+  (screenDisplay.innerHTML == "") {
+  secondDisplay.innerHTML == "";
+  } else
+  { secondDisplay.innerHTML= (firstNum) + " " + (operation) +  " " + (secondNum) 
+  }
+   secondNum = screenDisplay.innerHTML;
+      screenDisplay.innerHTML= calculateResult(operation)
+
+
+}})
 
 //clear button
 
@@ -73,15 +82,31 @@ clearcommand.addEventListener ("click", () =>
  
 
 // plusminus
-plusminuscommand.addEventListener ("click", () => {
- screenDigits = screenDisplay.innerHTML;
+const plusMinusFunction = () => {
 
-   if (screenDigits > 0) {
-   screenDisplay.innerHTML= screenDigits * (-1)
-   } else if (screenDigits < 0) {
-     screenDisplay.innerHTML = screenDigits * (1)
-   }
- }) 
+  if (screenDigits > 0) {
+   return screenDigits * (-1)
+    } else if (screenDigits < 0) {
+     return screenDigits * (1)
+    }
+}
+plusminuscommand.addEventListener ("click", () => {
+  screenDigits = screenDisplay.innerHTML;
+  screenDisplay.innerHTML= plusMinusFunction();
+})
+
+ 
+  
+// plusminuscommand.addEventListener ("click", () => {
+//  screenDigits = screenDisplay.innerHTML;
+
+//    if (screenDigits > 0) {
+//    screenDisplay.innerHTML= screenDigits * (-1)
+//    } else if (screenDigits < 0) {
+//      screenDisplay.innerHTML = screenDigits * (1)
+//    }
+//  }) 
+ 
 
 // percentages /100
 percentagecommand.addEventListener ("click", () => {
@@ -89,34 +114,3 @@ percentagecommand.addEventListener ("click", () => {
   screenDisplay.innerHTML= screenDigits / (100)}
 
 )
-// limit screen display to 10 digits for first and second numbers. 
-
-// {
-//   if (firstNum > 0) 
-//   screenDisplay.innerHTML= parseFloat (firstNum) * (-1) 
-//   else if ( firstNum < 0)
-//   screenDisplay.innerHTML= parseFloat (firstNum) * (1)  
-// }) 
-
-
-
-// //dot command
-// dotcommand.addEventListener ("click", () => {
-//   screenDisplay.innerHTML= firstNum || secondNum +  "."
-
-//   `// display jumber
-// // loop thro numbers
-
-
-
-
-// // symbols
-
-
-// //clear button
-
-// // strings to numbers
-
-// // let myNumbers = []
-// // let number = 
-// // myNumbers.push (number)
